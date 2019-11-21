9bd3f088476524733b9ad0c91f4e9e950b8dbb4d
yagilab2019 <56663125+yagilab2019@users.noreply.github.com>
2019-11-21 14:22:38 +0900

    Add files via upload

---
+++ look.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class look : MonoBehaviour {
+    public Transform verRot;
+    public Transform horRot;
+
+// Use this for initialization
+void Start () {
+    verRot = transform.parent;
+    horRot = GetComponent<Transform>();
+    }
+// Update is called once per frame
+
+void Update () {
+    float X_Rotation = Input.GetAxis("Mouse X");
+    float Y_Rotation = Input.GetAxis("Mouse Y");
+    verRot.transform.Rotate(0, -X_Rotation, 0);
+    horRot.transform.Rotate(Y_Rotation, 0, 0);
+    }
+}
