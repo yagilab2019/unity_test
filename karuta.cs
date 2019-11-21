bd7b7b54fc342ac18fa5c215139ac6d3f03d5456
yagilab2019 <56663125+yagilab2019@users.noreply.github.com>
2019-11-21 13:03:26 +0900

    Add files via upload

---
+++ karuta.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karuta : MonoBehaviour
+{
+    GameObject karuta_4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform myTransform = this.transform; 
+        Vector3 pos = myTransform.position;
+        if (Input.GetMouseButtonDown(0)) {
+ 
+            karuta_4 = null;
+ 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+ 
+            if (Physics.Raycast(ray, out hit)) {
+                karuta_4 = hit.collider.gameObject;
+            }
+            transform.position = new Vector3(0,0,7);
+            Debug.Log("もふもふえんもふもふ");
+        }
+    
+    }
+}
---
+++ karuta_spone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class karuta_spone : MonoBehaviour
+{
+    public GameObject irukaPrefab; //オリジナルのオブジェクト
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(irukaPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
